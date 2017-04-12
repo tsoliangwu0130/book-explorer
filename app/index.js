670cf43e4bc505fedf5a82c84714c2397645fd67
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-11 22:30:33 -0700

    Practice block scoping

--- app/index.js
+++ app/index.js
@@ -1,1 +1,12 @@
-console.log("Hello webpack (automatically update!)");
+function hello(){
+	let message = "Hello!";
+	console.log(message);
+}
+
+function greeting(){
+	let message = "How are you?"
+	console.log(message);
+}
+
+hello();
+greeting();
