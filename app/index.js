6ae7d6c80643034a03c2c07e5a16d5c6bced5de5
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-11 22:48:04 -0700

    Practice spread

--- app/index.js
+++ app/index.js
@@ -1,12 +1,10 @@
-function hello(){
-	let message = "Hello!";
-	console.log(message);
+let a = [20, 30, 40];
+let b = [10, ...a, 50];
+console.log(b);
+
+function collect(...a){
+	console.log(a);
 }
 
-function greeting(){
-	let message = "How are you?"
-	console.log(message);
-}
-
-hello();
-greeting();
+collect(1, 2, 3, 4, 5);
+collect('a', 2, 3, 'd');
