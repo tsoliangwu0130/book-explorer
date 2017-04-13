6971a4871384b0c70f03031c6e7814cd090bd496
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-12 21:23:26 -0700

    Practice spread

--- app/index.js
+++ app/index.js
@@ -1,9 +1,13 @@
+function print(text){
+	console.log(text)
+}
+
 let a = [20, 30, 40];
 let b = [10, ...a, 50];
-console.log(b);
+print(b);
 
 function collect(...a){
-	console.log(a);
+	print(a);
 }
 
 collect(1, 2, 3, 4, 5);
