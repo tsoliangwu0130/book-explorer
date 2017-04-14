ad80f8989956a556e82f1ad5eef50e130c81fdcf
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-13 22:36:56 -0700

    Practice destructure array

--- app/index.js
+++ app/index.js
@@ -1,14 +1,3 @@
-function print(text){
-	console.log(text)
-}
-
-let a = [20, 30, 40];
-let b = [10, ...a, 50];
-print(b);
-
-function collect(...a){
-	print(a);
-}
-
-collect(1, 2, 3, 4, 5);
-collect('a', 2, 3, 'd');
+let z = [4, 5, 6];
+let [a, b] = z;
+console.log(a, b);
