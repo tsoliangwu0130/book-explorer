c8120dc466555b5fcc68f12827c32cba664f508b
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-18 11:24:58 -0700

    Practice filter/map

--- app/index.js
+++ app/index.js
@@ -1,4 +1,9 @@
-let array = [10, 15, 6, 7, 17, 18, 20];
-let higher_score = array.filter((n) => n > 15);
+let array_01 = [10, 20, 30];
+let doubled_array = array_01.map((n) => n * 2);
 
+
+let array_02 = [10, 15, 6, 7, 17, 18, 20];
+let higher_score = array_02.filter((n) => n > 15);
+
+console.log(doubled_array);
 console.log(higher_score);
