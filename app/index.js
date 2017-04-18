a1637ceeea4ae10f4b2dff448e744b047c24d7d8
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-17 23:20:14 -0700

    Practice function mapping

--- app/index.js
+++ app/index.js
@@ -1,17 +1,8 @@
-function cheer_01(){
-	console.log("Hello ES6 from cheer_01!");
+let array = [10, 20, 30];
+
+let double = (n) => {
+	return n * 2;
 }
 
-
-let cheer_02 = function(){
-	console.log("Hello ES6 from cheer_02!");
-}
-
-// Anonymous Function Style
-let cheer_03 = () => {
-	console.log("Hello ES6 from cheer_03!");
-}
-
-cheer_01();
-setTimeout(cheer_02, 3000);
-setTimeout(cheer_03, 5000);
+let doubled = array.map(double);
+console.log(doubled);
