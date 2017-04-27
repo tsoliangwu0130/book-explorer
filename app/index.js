2f9805b359c170f30703ac19b59885e87d0fcee1
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-27 15:03:37 -0700

    Practice static class

--- app/index.js
+++ app/index.js
@@ -1,16 +1,8 @@
-import Animal from "./animal";
-
-class Lion extends Animal {
-	constructor(name, height, color) {
-		super(name, height);
-		this.color = color;
-	}
-
-	greeting() {
-		console.log(`Hi, I'm ${ this.name }!`);
+class Calculator {
+	static multiply(a, b) {
+		return a * b;
 	}
 }
 
-let son = new Lion("Simba", 2, 'golden');
-console.log(son);
-son.greeting();
+let a = Calculator.multiply(5, 7);
+console.log(a);
