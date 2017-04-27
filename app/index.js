3dd4b443ad1637a9e23db6869109f62fea683e55
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-27 15:04:14 -0700

    Practice static class

--- app/index.js
+++ app/index.js
@@ -2,7 +2,12 @@ class Calculator {
 	static multiply(a, b) {
 		return a * b;
 	}
+
+	static add(a, b) {
+		return a + b;
+	}
 }
 
 let a = Calculator.multiply(5, 7);
-console.log(a);
+let b = Calculator.add(5, 7);
+console.log(a, b);
