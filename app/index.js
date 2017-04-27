eb0fbd62fda029e9efa5edeb73f25590088dfd6e
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-26 22:50:57 -0700

    Practice build-in function

--- app/index.js
+++ app/index.js
@@ -1,9 +1,3 @@
-let array_01 = [10, 20, 30];
-let doubled_array = array_01.map((n) => n * 2);
+let b = "wooh" + "oo".repeat(50);
 
-
-let array_02 = [10, 15, 6, 7, 17, 18, 20];
-let higher_score = array_02.filter((n) => n > 15);
-
-console.log(doubled_array);
-console.log(higher_score);
+console.log(b);
