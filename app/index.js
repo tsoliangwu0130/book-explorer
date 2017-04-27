dfc3b1b705757f3825e1d4fe07a98711be69ee3d
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-26 23:09:22 -0700

    Practice build-in function

--- app/index.js
+++ app/index.js
@@ -3,3 +3,14 @@ let word = "hello world";
 console.log(word.startsWith("hello"));
 console.log(word.endsWith("world"));
 console.log(word.includes("llo w"));
+
+console.log("===========");
+
+const addToCart = (item, number) => {
+	console.log(number, "isFinite", Number.isFinite(number));
+	console.log(number, "isSafe", Number.isSafeInteger(number));
+} 
+
+addToCart("shoes", 5);
+addToCart("shoes", Infinity);
+addToCart("shoes", Math.pow(2, 54));
