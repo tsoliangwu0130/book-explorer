29d2d57e1eae88cfb1c74cff2de0dec61bbb4a64
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-26 23:16:30 -0700

    Practice import/export module

--- app/index.js
+++ app/index.js
@@ -1,16 +1,3 @@
-let word = "hello world";
+import { arrayFromModule } from './testmodule';
 
-console.log(word.startsWith("hello"));
-console.log(word.endsWith("world"));
-console.log(word.includes("llo w"));
-
-console.log("===========");
-
-const addToCart = (item, number) => {
-	console.log(number, "isFinite", Number.isFinite(number));
-	console.log(number, "isSafe", Number.isSafeInteger(number));
-} 
-
-addToCart("shoes", 5);
-addToCart("shoes", Infinity);
-addToCart("shoes", Math.pow(2, 54));
+console.log(arrayFromModule);
---
+++ app/testmodule.js
@@ -0,0 +1,2 @@
+const arrayFromModule = ['a', 'b', 'c'];
+export { arrayFromModule };
