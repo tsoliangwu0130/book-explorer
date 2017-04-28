71bd856a92781dd828825d52f873c481ea1767e6
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-28 16:46:43 -0700

    Closure practice

--- app/index.js
+++ app/index.js
@@ -1,13 +1,9 @@
-let strings = 'as;fhjsldahlhfhasdhfluahuhr';
-let letters = new Map();
-
-for (let i=0; i<strings.length; i++) {
-	let letter = strings[i];
-	if (!letters.has(letter)) {
-		letters.set(letter, 1);
-	} else {
-		letters.set(letter, letters.get(letter) + 1);
+let call = () => {
+	let secret = "ES6 rocks!";
+	let revel = () => {
+		console.log(secret);
 	}
+	revel();
 }
 
-console.log(letters);
+call();
