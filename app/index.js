9c75b1dd1cb3e96e0a473febc2c7f6b404442f5e
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-28 15:50:06 -0700

    Map practice

--- app/index.js
+++ app/index.js
@@ -1,5 +1,10 @@
-let chars = 'aasdashjahlfhuhfuiqwhebjqwvlasfmvnjbj';
-let chars_arr = chars.split("");
-let chars_set = new Set(chars_arr);
+let a = new Map();
+let key_1 = "string key"
+let key_2 = { a: 'key' };
+let key_3 = function() {};
 
-console.log(chars_set);
+a.set(key_1, 'return value from a string key');
+a.set(key_2, 'return value from an object key');
+a.set(key_3, 'return value from a function key');
+
+console.log(a);
