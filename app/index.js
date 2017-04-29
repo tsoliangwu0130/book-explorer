4bd416861bd0751bcdf3d3c31dd584df107a1f02
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-28 17:24:51 -0700

    Private practice

--- app/index.js
+++ app/index.js
@@ -4,16 +4,18 @@ const budge = () => {
 		return balance += val;
 	}
 	const deposit20 = () => changeBal(20);
+	const withdraw20 = () => changeBal(-20);
 	const check = () => balance;
 
-	return {
-		deposit20: deposit20,
-		check: check
-	}
+	return { deposit20, withdraw20, check }
 }
 
 let wallet = budge();
 console.log(wallet);
 wallet.deposit20();
+wallet.deposit20();
+wallet.withdraw20();
+wallet.deposit20();
+wallet.withdraw20();
 console.log(wallet.check());
 console.log(wallet.balance); // undefined!
