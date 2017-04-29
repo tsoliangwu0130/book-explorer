bed487e1b5c7fbc320aa02539a2f615f06ce1383
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-28 21:26:01 -0700

    REST API fetch practice

--- app/index.js
+++ app/index.js
@@ -1,6 +1,5 @@
-let p = new Promise((resolve, reject) => {
-	setTimeout(() => resolve('Resolved promise data'), 3000);
-});
+const root = 'https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699';
 
-p.then(response => console.log(response))
- .catch(error => console.log(error));
+fetch(root, { method: 'GET' })
+	.then(response => response.json())
+	.then(json => console.log(json));
