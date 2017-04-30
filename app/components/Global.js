d96a4dacb17e3316fca44e56b4a8a56905e6cec8
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-29 22:48:51 -0700

    Update bootstrap link

--- app/components/Global.js
+++ app/components/Global.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class Global extends Component {
     render() {
         return (
-            <div>The Global App Component!</div>
+            <div>
+                <h2>Book Explorer!</h2>
+                <FormGroup>
+                    <InputGroup>
+                        <FormControl type="text" placeholder="Search for a book" />
+                        <InputGroup.Addon>
+                            <Glyphicon glyph="search"></Glyphicon>
+                        </InputGroup.Addon>
+                    </InputGroup>
+                </FormGroup>
+            </div>
         )
     }
 }
