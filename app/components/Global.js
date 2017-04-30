e75a81891f4d18edc677bd1b496177845dc2bed4
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-29 22:52:08 -0700

    Add onclick listener

--- app/components/Global.js
+++ app/components/Global.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class Global extends Component {
+    search() {
+        console.log('search');
+    }
+
     render() {
         return (
             <div>
@@ -9,7 +13,7 @@ class Global extends Component {
                 <FormGroup>
                     <InputGroup>
                         <FormControl type="text" placeholder="Search for a book" />
-                        <InputGroup.Addon>
+                        <InputGroup.Addon onClick={() => this.search()}>
                             <Glyphicon glyph="search"></Glyphicon>
                         </InputGroup.Addon>
                     </InputGroup>
