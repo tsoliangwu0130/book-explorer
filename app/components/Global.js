319b88005f6c7228007395820aa020589c80c416
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-29 23:28:44 -0700

    State practice

--- app/components/Global.js
+++ app/components/Global.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class Global extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: ''
+        }
+    }
+
     search() {
-        console.log('search');
+        console.log('search', this.state.query);
     }
 
     render() {
@@ -12,7 +19,16 @@ class Global extends Component {
                 <h2>Book Explorer!</h2>
                 <FormGroup>
                     <InputGroup>
-                        <FormControl type="text" placeholder="Search for a book" />
+                        <FormControl
+                            type="text"
+                            placeholder="Search for a book"
+                            onChange={event => this.setState({query: event.target.value})}
+                            onKeyPress={event => {
+                                if (event.key === 'Enter') {
+                                    this.search();
+                                }
+                            }}
+                        />
                         <InputGroup.Addon onClick={() => this.search()}>
                             <Glyphicon glyph="search"></Glyphicon>
                         </InputGroup.Addon>
