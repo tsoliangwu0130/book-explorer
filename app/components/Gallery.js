30a88d5124957d89024bc5ca692dd11712d55795
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-04-30 21:26:39 -0700

    Add gallery style

--- app/components/Gallery.js
+++ app/components/Gallery.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react';
 
 class Gallery extends Component {
     render() {
+        let alternate = 'https://pbs.twimg.com/profile_images/600060188872155136/st4Sp6Aw.jpg'
+
         return(
             <div>
                 {
                     this.props.items.map((item, index) => {
-                        let { title } = item.volumeInfo;
+                        let { title, imageLinks, infoLink } = item.volumeInfo;
                         return(
-                            <div key={ index }>{ title }</div>
+                            <a
+                                href={ infoLink }
+                                target="_blank"
+                                key={ index }
+                                className="book"
+                            >
+                                <img
+                                    src={ imageLinks !== undefined ? imageLinks.thumbnail : alternate}
+                                    alt="book image"
+                                    className="book-img"
+                                />
+                                <div className="book-text">
+                                    { title }
+                                </div>
+                            </a>
                         )
                     })
                 }
